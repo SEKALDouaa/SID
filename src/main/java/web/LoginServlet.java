6a53f47b48bad1a812102ac44f481f1053df76da
Douaa <douaasekal@gmail.com>
2024-05-11 11:45:21 +0100

    Update web package from previous project (Add login and register servlets)

---
+++ src/main/java/web/LoginServlet.java
@@ -0,0 +1,46 @@
+package web;
+
+import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class LoginServlet extends HttpServlet {
+	
+
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        doPost(req,resp);
+    }
+    
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+    	String email = req.getParameter("email");
+        String pwd = req.getParameter("pwd");
+
+        RequestDispatcher dispatcher=null;
+       
+        System.out.println(pwd+" "+ email);
+
+        DataBase db = new DataBase(new MySQLDataSource("servlet"));
+        User user =(User)db.selectElement(email, pwd, User.class);
+        HttpSession session = req.getSession();
+        System.out.println(user);
+        if("admin@gmail.com".equals(user.getEmail()) && "admin".equals(user.getPwd())){
+        	session.setAttribute("user", user);
+            dispatcher=req.getRequestDispatcher("admin.jsp");
+        }
+        
+        else if (user.getId() != 0) {
+            session.setAttribute("user", user);
+            dispatcher=req.getRequestDispatcher("home.jsp");
+        }
+        else {
+            session.setAttribute("error", "Authentication failed. Please try again.");
+            dispatcher=req.getRequestDispatcher("index.jsp");
+        }
+        dispatcher.forward(req, resp);
+	}
+}
+    
---
+++ src/main/java/web/RegisterServlet.java
@@ -0,0 +1,47 @@
+package web;
+
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class RegisterServlet extends HttpServlet{
+	
+	protected void doPost(HttpServletRequest req, HttpServletResponse resp) {
+		try {
+			System.out.println("Execution d'un service");
+			String name=req.getParameter("name");
+			String email=req.getParameter("emailu");
+			String pwd=req.getParameter("pwdu");
+			
+			
+			System.out.println(name+" "+pwd+" "+ email);
+			
+			
+			DataBase db=new DataBase(new MySQLDataSource("servlet"));
+			
+			RequestDispatcher dispatcher=null;
+			
+			HttpSession session = req.getSession();
+			
+			int insert=db.insertElement(new User(name,email,pwd));
+			
+			if(insert>0) {
+				session.setAttribute("success", "You have been registered successfully!");
+				dispatcher=req.getRequestDispatcher("index.jsp");
+			}
+			else {
+				req.setAttribute("failed", "An error has occured");
+				dispatcher=req.getRequestDispatcher("index.jsp");
+			}
+
+			dispatcher.forward(req, resp);
+		}catch(Exception e) {
+			e.printStackTrace();
+		}
+		
+	}
+	
+	protected void doGett(HttpServletRequest req, HttpServletResponse resp) {
+		doPost(req,resp);
+	}
+}
